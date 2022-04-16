b65bf6a75a3018e8307360d5cc75e5157bc6e90d
drvram <fakirsoft@yandex.ru>
2022-04-16 10:26:01 +0500

    add comments for get_post

--- clsss_of_vk.py
+++ clsss_of_vk.py
@@ -11,63 +11,69 @@ from data.auth_data.auth_vk import access_token4, access_token2, headers
 class LegalVKParser:
     tasks = []
 
-    def __init__(self, *tokens):
-        self.tokens_tuple = tokens
-        self.check_auth(self.tokens_tuple)
+    def __init__(self, *tokens):  # объявляем конструктор класса
+        self.tokens_tuple = tokens  # принимаем произвольное количество токенов, всё это будет кортежем
+        self.check_auth(self.tokens_tuple)  # и проверяем их на работоспособность
 
-    def check_auth(self, tokens_tuple):
+    def check_auth(self, tokens_tuple):  # проверка токенов
         """
         проверка токена
         tokens_tuple: param tokens_tuple: токен пользователя, tuple
         """
-        if self.tokens_tuple:
-            auth_error_list = []
-            tokens_list = []
-            for token in tokens_tuple:
-                while True:
-                    token_number = tokens_tuple.index(token) + 1
-                    print(f'Проверяю токен номер {token_number}')
+        if self.tokens_tuple:  # если токены переданы
+            auth_error_list = []  # объявляем лист, в который будут записываться индексы не рабочих токенов
+            tokens_list = []  # ну и новый лист для рабочих токенов
+            for token in tokens_tuple:  # перебираем токены
+                while True:  # запускаем бесконечный цикл на случай не удачных авторизаций не по 5 ошибке
+                    token_number = tokens_tuple.index(token) + 1  # получаем индекс текущего
+                    print(f'Проверяю токен номер {token_number}')  # пользовательский вывод
                     check_auth_url = f'https://api.vk.com/method/wall.get?count=1&access_token={token}&v=5.131'
-                    req = requests.Session()
-                    req = req.get(url=check_auth_url, headers=headers)
-                    if 'error' in req.text[0:9]:
-                        result = json.loads(req.text)
-                        code_error, print_error = self.vk_errors(result)
-                        print(f'Токен номер {token_number} не сработал, код ошибки {code_error}')
+                    # ссылка на проверку текущий авторизации, просто пытаемся получить первую запись со своей страницы
+                    req = requests.Session()  # откарываем обычную синхронную сессию
+                    req = req.get(url=check_auth_url, headers=headers)  # ну и запрашиваем данных
+                    if 'error' in req.text[0:9]:  # если видим ошибку
+                        result = json.loads(req.text)  # парсим всё полученное в словарь
+                        code_error, print_error = self.vk_errors(result)  # и получаем текст и код ошибки (об этом ниже)
+                        print(f'Токен номер {token_number} не сработал, код ошибки {code_error}')  # сообщаем
                         print('Текст ошибки')
-                        print(print_error)
+                        print(print_error)  # также
                         print()
-                        if code_error == 5:
+                        if code_error == 5:  # и вот если ошибка 5, не удачная авторизацияЮ добовляем номер текущего
+                            # токена в список "битых" токенов
                             auth_error_list.append(token_number)
-                            break
+                            break  # и прерываем цикл
                         else:
                             print('Ждем 3 сек и продолжаем...')
-                            time.sleep(3)
+                            time.sleep(3)  # если ошибка не 5 (может с инетом беда или превышено количество запросов)
+                            # отдыхаем 3 сек и пробуем ещё раз
+                            # если в токине будет ошибка написания, то он полюбому выбивает 5 ошибку, поэтому на тестах
+                            # зацикливание исключено, но доработаю ещё на ручное прерывание
                             continue
                     else:
-                        print(f'Токен номер {token_number} ок.')
+                        print(f'Токен номер {token_number} ок.')  # сообщаем об успешности проверки токена
                         print()
-                        tokens_list.append(token)
-                        break
-            if auth_error_list:
+                        tokens_list.append(token)  # и добавляем токен в новый список для переопределение листа
+                        break  # также прерываем цикл
+            if auth_error_list:  # если в списке битых токенов хоть чтонибудь есть
                 print(f'Токены под номерами {auth_error_list} не прошли проверку авторизации, желаете продолжить?')
-                while True:
+                while True:  # запрашиваем ввод пользователя, продолжать или нет, т.к. не все токены прошли проверку
                     input_exit = input('y - продолжить, n - выход ').lower()
                     if input_exit == 'y':
                         break
                     elif input_exit == 'n':
                         sys.exit(0)
-            self.tokens_tuple = tuple(tokens_list)
+            self.tokens_tuple = tuple(tokens_list)  # переопределяем список и записываем обратно в свойство класса
             print('Проверка токенов завершена, кортеж токенов переопределён')
         else:
             raise ValueError("Необходимо передать токены, при создании объекта класса")
+            # исключение на случай если при создании экземпляра класса не было передано ни одного токена
 
     @staticmethod
     def vk_errors(result):  # получение текста ошибки по коду ошибки
-        error_code = result['error']['error_code']
+        error_code = result['error']['error_code']  # раз метод вызван, то точно ошибка и смело получаем её код
         with open('json_data/vk_api_errors.json', encoding='utf-8') as file:
-            vk_api_errors = json.load(file)
-        return error_code, vk_api_errors[str(error_code)]
+            vk_api_errors = json.load(file)  # парсим из файла подготовленный словарь ошибок в вк
+        return error_code, vk_api_errors[str(error_code)]  # и по нему возвращаем текст и код ошибки
 
     def get_post_id(self, group_id):
         """
@@ -85,33 +91,35 @@ class LegalVKParser:
             :param get_post_list_off_set:
             :return:
             """
-            post_id_list_post_list = []
+            post_id_list_post_list = []  # внутренний лист для записи лайков из поста
             url_post_list = f'https://api.vk.com/method/wall.get?owner_id={group_id}&' \
                             f'offset={str(get_post_list_off_set)}&count=100&offset={str(get_post_list_off_set)}' \
-                            f'&access_token={token}&v=5.131'
-            async with aiohttp.ClientSession() as session:
-                req_post_list = await session.get(url=url_post_list, headers=headers)
-                result_post_list = json.loads(await req_post_list.text())
-                while True:
-                    try:
+                            f'&access_token={token}&v=5.131'  # формируем ссылку
+            async with aiohttp.ClientSession() as session:  # асинхронно открываем сессию
+                req_post_list = await session.get(url=url_post_list, headers=headers)  # запарашиваем данные
+                result_post_list = json.loads(await req_post_list.text())  # парсим их в словарь
+                while True:  # обробатываем полученные данные бесконечных циклом для повторных попыток (ОШИБКА)
+                    try:  # открываем try, для обработки слловаря
                         for item in result_post_list['response']['items']:
                             post_id_list_post_list.append(item['id'])
+                            # если такие поля есть, значит мы спокойно получаем id поста и идём дальше завершая цикл
                         break
-                    except KeyError:
+                    except KeyError:  # если такого поля нет
                         print('Возникла ошибка')
                         error_code_post_list, error_text_error_code_post_list = self.vk_errors(result_post_list)
-                        if error_code_post_list == 5:
+                        # логично пологать, что значит ошибка и мы прогоняем её через функцию обработки ошибок
+                        if error_code_post_list == 5:  # а если ошибка авторизации то завершаем работу (ПЕРЕДЕЛАТЬ)
                             print('Произошла ошибка авторизации, используемый токен более не действителен')
                             print('Работа приложения завершена, все данные потеряны :)')
-                            sys.exit(0)
+                            sys.exit(0)  # выход
                         else:
-                            print(f'Код ошибки {error_code_post_list}')
-                            print(error_text_error_code_post_list)
+                            print(f'Код ошибки {error_code_post_list}')  # если это не ошибка авторизации
+                            print(error_text_error_code_post_list)  # распечатываем данные и пробуем ещё
                             print('Ждем пару секунд и пробуем ещё раз')
 
-            await asyncio.sleep(1)
-            print('...', end='')
-            nonlocal post_id_list
+            await asyncio.sleep(1)  # спим, чтоб не превышать количество запросов
+            print('...', end='')  # пользовательская загрузка для прогресса
+            nonlocal post_id_list  # берем лист из внешней функции и пополняем его итоговым списком постов
             post_id_list += post_id_list_post_list
 
         async def tasks_for_posts_id(group_id):
@@ -120,33 +128,34 @@ class LegalVKParser:
             :param group_id:
             :return:
             """
-            tasks = []
+            tasks = []  # очищаем список тасков (малоли что там было)
             print('Получаем количество постов:', end=' ')
             url_count = f'https://api.vk.com/method/wall.get?owner_id={group_id}' \
                         f'&count=1&access_token={self.tokens_tuple[0]}&v=5.131'
-            req_count = requests.Session()
+            # формируем url для получения количества постов
+            req_count = requests.Session()  # обычных реквестом запрашиваем первую запись в которой указано количество
             req_count = req_count.get(url=url_count, headers=headers)
             result_count = json.loads(req_count.text)
-            print(result_count['response']['count'])
+            print(result_count['response']['count'])  # парсим в словарь
             print('Получаем посты...', end='')
-            count_iterations = result_count['response']['count'] // 100 + 1
-            post_id_off_set = 0
-            post_id_k = 0
-            while post_id_k <= count_iterations:
-                for token in self.tokens_tuple:
-                    for page in range(1, 4):
+            count_iterations = result_count['response']['count'] // 100 + 1  # считаем количество итераций
+            post_id_off_set = 0  # объявялем (и обнуляем смещение)
+            post_id_k = 0  # а эт чтоб по циклу двигаться
+            while post_id_k <= count_iterations:  # поехали
+                for token in self.tokens_tuple:  # перебираем все имеющиеся токены чтобы запустить парсинг
+                    for page in range(1, 4):  # каждый токен получает по три страницы (ограничения вк)
                         task = asyncio.create_task(get_post_list(group_id, token, post_id_off_set))
                         tasks.append(task)
-                        post_id_off_set += 100
-                        post_id_k += 1
-                await asyncio.gather(*tasks)
+                        post_id_off_set += 100  # смещаем оффсет
+                        post_id_k += 1  # и счетчик
+                await asyncio.gather(*tasks)  # ну и когда задачи сформированы, ждём их выполнения
 
-        asyncio.run(tasks_for_posts_id(group_id=group_id))
+        asyncio.run(tasks_for_posts_id(group_id=group_id))  # основной запуск всего этого дела
         print('ок')
         print('Посты получены.')
-        with open(f'{group_id}_id_posts.json', 'w') as file:
+        with open(f'{group_id}_id_posts.json', 'w') as file:  # записываем всё в файл
             json.dump(post_id_list, file, indent=4, ensure_ascii=False)
-        return post_id_list
+        return post_id_list  # а эт передача листа для дальнейшей работы
 
     def get_likes_from_group(self, group_id):
         """
