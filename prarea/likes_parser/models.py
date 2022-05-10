1e63c8ad51203e59b307b8f57c95b74b05f730db
drvram <fakirsoft@yandex.ru>
2022-05-11 00:57:21 +0500

    add model GroupList initial settings app

--- prarea/likes_parser/models.py
+++ prarea/likes_parser/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class GroupList(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название группы',
+                             help_text='Введите название группы для отображения в списке')
+    group_id = models.IntegerField(default=0, verbose_name='ID Группы')
+    data = models.JSONField(null=False, default=dict, blank=True, verbose_name='Json данные')
+    user_id = models.CharField(max_length=2, verbose_name='Тест поле для юзера', default=0, blank=True)
+
+    def __str__(self):
+        return str(self.group_id)
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
--- prarea/likes_parser/views.py
+++ prarea/likes_parser/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class Home(ListView):
+    model = GroupList
+    template_name = 'likes_parser/home.html'
+    context_object_name = 'parser'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['main'] = 'Hello from parser'
+        return context
